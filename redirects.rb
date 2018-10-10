312ac68ba90cfe8d2efa3df73d6be026f277b90c
Karen Miller <kmiller@pivotal.io>
2018-10-10 13:41:43 -0700

    Add PCC v1.5.0 to cf-services PCC build [#160325039]

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/p-cloud-cache/(?![\d-]+)(.*)}, "/p-cloud-cache/1-4/$1"
+r301 %r{/p-cloud-cache/(?![\d-]+)(.*)}, "/p-cloud-cache/1-5/$1"
