36448d1b37297fb1c8680894003e8f529f1a64c0
Samia Nneji <snneji@pivotal.io>
2018-11-15 16:39:59 +0000

    Update redirects for PDFs [#160895280]

--- redirects.rb
+++ redirects.rb
@@ -4,3 +4,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 r301 %r{/p-cloud-cache/(?![\d-]+)(.*)}, "/p-cloud-cache/1-5/$1"
+
+# Redirect old versions to PDFs
+r301 %r{/p-cloud-cache/1-0/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.0.8.pdf'
+r301 %r{/p-cloud-cache/1-1/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.1.5.pdf'
+r301 %r{/p-cloud-cache/1-2/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.2.2.pdf'
