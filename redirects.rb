204d593e88efea8b5380b061614954ffb3ed9529
Karen Miller <kmiller@pivotal.io>
2019-04-02 16:11:31 -0700

    Update edge/master to be for PCC v1.8 [#165067121]

--- redirects.rb
+++ redirects.rb
@@ -1,3 +1,3 @@
 #Redirect paths without version number and are partial images to image
 
-r301 %r{/p-cloud-cache/(?![\d-]|[o][d][b]+)(.*)}, "/p-cloud-cache/1-7/$1"
+r301 %r{/p-cloud-cache/(?![\d-]|[o][d][b]+)(.*)}, "/p-cloud-cache/1-8/$1"
