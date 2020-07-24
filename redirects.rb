712d9aab28a7aca25e2ff19d213529b7575770af
Karen Miller <kmiller@pivotal.io>
2020-07-24 11:32:03 -0700

    Update master/edge to be for TGF[VMs]1.13.

--- redirects.rb
+++ redirects.rb
@@ -4,7 +4,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 #Redirect paths without version number and are partial images to image
-r301 %r{/p-cloud-cache/(?![\d-]|[p][a][r][t]+)(.*)}, "/p-cloud-cache/1-12/$1"
+r301 %r{/p-cloud-cache/(?![\d-]|[p][a][r][t]+)(.*)}, "/p-cloud-cache/1-13/$1"
 
 # Redirect old versions to PDFs
 r301 %r{/p-cloud-cache/1-0/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.0.8.pdf'
