10946ee9149d43f50208e2bd7962218328645143
Dave Barnes <dbarnes@apache.org>
2022-04-15 17:08:28 -0700

    update redirects to vmware

--- redirects.rb
+++ redirects.rb
@@ -4,10 +4,12 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 #Redirect paths without version number and are partial images to image
-r301 %r{/p-cloud-cache/(?![\d-]|[p][a][r][t]+)(.*)}, "/p-cloud-cache/1-14/$1"
+#r301 %r{/p-cloud-cache/(?![\d-]|[p][a][r][t]+)(.*)}, "https://docs.vmware.com/en/VMware-Tanzu-GemFire-for-VMs/1.14/tgf-vms/GUID-content-index.html"
+r301 %r{/p-cloud-cache/1-14/(.*)}, "https://docs.vmware.com/en/VMware-Tanzu-GemFire-for-VMs/1.14/tgf-vms/GUID-content-index.html"
 
 # Redirect old versions to PDFs
 r301 %r{/p-cloud-cache/1-0/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.0.8.pdf'
 r301 %r{/p-cloud-cache/1-1/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.1.5.pdf'
 r301 %r{/p-cloud-cache/1-2/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.2.2.pdf'
 r301 %r{/p-cloud-cache/1-3/(.*)}, 'http://docs.pivotal.io/archives/p-cloud-cache-1.3.5.pdf'
+
